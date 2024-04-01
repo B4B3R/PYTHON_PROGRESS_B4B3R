9ff3e24fdba1d5289e2c2e3d4eeddda52576552c
B4B3R <162824906+B4B3R@users.noreply.github.com>
2024-04-01 20:06:42 +0200

    Create True_or_False.py

---
+++ True_or_False.py
@@ -0,0 +1,5 @@
+Object_1 = str(input("What's your sentence?"))
+Object_2 = int(input("What's your number?"))
+
+print("For ", str(type(Object_1)),bool(Object_1))
+print("For ", str(type(Object_2)), bool(Object_2))
