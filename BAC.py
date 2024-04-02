8a419e63a3a881da91f97e6f72bcc244aa55cc80
B4B3R <162824906+B4B3R@users.noreply.github.com>
2024-04-02 13:36:47 +0200

    Update BAC.py

--- BAC.py
+++ BAC.py
@@ -2,11 +2,13 @@ MA_NOTE = int(input("Entrez votre moyenne."))
 
 if MA_NOTE < 12:
     print("Mention : Pas de mention")
-elif MA_NOTE >= 12:
+elif 12 <= MA_NOTE < 14:
     print("Mention : Assez bien")
-elif MA_NOTE >= 14:
+elif 14 <= MA_NOTE and MA_NOTE < 16:
     print("Mention : Bien")
-elif MA_NOTE >= 16:
+elif 16 <= MA_NOTE < 18:
     print("Mention : Très bien")
-elif MA_NOTE >= 18:
+elif 18 <= MA_NOTE <= 20:
     print("Mention : Les Félicitations du jury")
+else:
+    print("Votre moyenne ne peut être supérieur à 20.")
