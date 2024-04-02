770edb30b1138016ff2251f828f6b111157de543
B4B3R <162824906+B4B3R@users.noreply.github.com>
2024-04-02 16:47:36 +0200

    correction in the loop

--- Small-Projects/I_WANT_PIZZA.py
+++ Small-Projects/I_WANT_PIZZA.py
@@ -4,31 +4,59 @@ my_table = []
 my_wallet = 13.5
 print("Respond with yes or no")
 print("You have " + str(my_wallet) + " dollars in your wallet")
-question = str(input("Do you want a pizza ?"))
+counter = 0
 
-if question == "yes":
-    pizza_question = str(input("Which pizza do you want ?"))
-    if pizza_question == "Salmone Pizza":
-        print("Give me 10.5 dollars, please")
-        my_wallet -= 10.5
-        print("One pizza salmone for this table.")
-        my_table.append("pizza salmone")
-    elif pizza_question == "Sicilian Pizza":
-        print("Give me 10.5 dollars, please")
-        my_wallet -= 10.5
-        print("One Sicilian Pizza  for this table.")
-        my_table.append("Sicilian Pizza")
-    elif pizza_question == "Roman Pizza":
-        print("Give me 10.5 dollars, please")
-        my_wallet -= 10.5
-        print("One Roman Pizza for this table.")
-        my_table.append("Roman Pizza")
+while counter < 3:
+    
+    question = str(input("Do you want a pizza ?     ")) # the question is inside the loop because you need to "refresh" the loop
+
+    if question == "yes":
+
+        pizza_question = str(input("Which pizza do you want ?   "))
+
+        if pizza_question == "Salmone Pizza":
+
+            print("Give me 10.5 dollars, please")
+            my_wallet -= 10.5
+            print("One pizza salmone for this table.")
+            my_table.append("pizza salmone")
+            counter += 1
+            continue
+
+        elif pizza_question == "Sicilian Pizza":
+
+            print("Give me 10.5 dollars, please")
+            my_wallet -= 10.5
+            print("One Sicilian Pizza  for this table.")
+            my_table.append("Sicilian Pizza")
+            counter += 1
+            continue
+
+        elif pizza_question == "Roman Pizza":
+
+            print("Give me 10.5 dollars, please")
+            my_wallet -= 10.5
+            print("One Roman Pizza for this table.")
+            my_table.append("Roman Pizza")
+            counter += 1
+            continue
+
+        else:
+
+            print("We just have the :\n     -Roman Pizza\n     -Sicilian Pizza\n     -Salmone Pizza")
+            counter += 1
+            continue
+
+    elif question == "no":
+
+        print("Ok, what are you doing in that restaurant.")
+        break
+
     else:
-        print("We just have the :\n     -Roman Pizza\n      -Sicilian Pizza\n       -Salmone Pizza") 
-elif question == "no":
-    print("Ok, what are you doing in that restaurant.")
-else:
-    print("Please respond with yes or no")
+
+        print("Please respond with yes or no")
+        continue
 
 print("You have " + str(my_wallet) + " dollars in your wallet")
-
+print("In your table you have, " + str(my_table))
+print(counter)
