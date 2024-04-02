d7efadefa0e9c9ad49f2c6c75299f8da51b63e43
B4B3R <162824906+B4B3R@users.noreply.github.com>
2024-04-02 22:02:13 +0200

    discovery of the function range()

--- FUNCTIONS_USECASE/Random()/prog.py
+++ FUNCTIONS_USECASE/Random()/prog.py
@@ -1,1 +1,41 @@
+# First usecase of the function random()
 
+from random import * # or just import random but if you do that you need to call function random.random() not just random
+
+a = random()
+print(a)
+
+list = []
+# range() : 
+
+for l in range(10):
+    list.append(l)
+    print(list)
+
+for l in range(0,8):
+    list.remove(l)
+    print(list)
+# the code below is the the same
+#for l in range(start=0,stop=8):
+#     list.remove(l)
+#     print(list)
+    
+
+for k in range(0,10,2): # range(start = 0, stop = 10, step = 2)
+    list.count(k)
+    print(list)
+
+
+x = range(6)
+
+for n in x:
+    print(n)
+
+print(x)
+
+y = range(9)
+
+for n in y:
+    print(n)
+
+print(y)
